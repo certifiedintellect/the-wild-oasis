608bce1a2622434957b34b8111aca4168f64d5b5
nikhil <nikhilmara123@gmail.com>
2024-02-27 20:19:45 +0530

    added supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.log(error);
+    throw new Error("Unable to fetch cabins");
+  }
+
+  return data;
+}
